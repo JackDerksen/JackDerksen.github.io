96833f6bd4bef57b25bd57de900d7b5381dc4bbb
Jack Derksen <jmderksen9@gmail.com>
2024-12-29 19:08:09 -0700

    Added react integration to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   output: 'static',
   site: 'https://JackDerksen.github.io',
+  integrations: [react()],
 });
